6236cb328823fbee45ff692c41e060e8988a4e4d
Laura <laura.maser3@yahoo.com>
2022-03-17 14:05:42 -0400

    Started component for comment form modal.

---
+++ react-app/src/components/Comments/CommentForm/CommentForm.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
+
+import { createComment } from "../../../store/comment";
+import './CommentForm.css';
+
+const CommentForm = ({ onClose }) => {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const { id } = useParams();
+
+    const user = useSelector(state => state.session.user);
+    const post = useSelector(state => state.posts[id]);
+
+    const [ errors, setErrors ] = useState([]);
+    const [ body, setBody ] = useState('');
+
+    const updateBody = (e) => setBody(e.target.value);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        // const payload = {};
+
+        // const newComment = await dispatch(createComment(payload));
+
+        // if (newComment.ok) history.push();
+    };
+
+    const handleCancel = (e) => {
+        e.preventDefault();
+        // history.push();
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <ul>
+                {errors.map((error, ind) => <li key={ind}>{error}</li>)}
+            </ul>
+            <input
+                type='textarea'
+                name='body'
+                value={body}
+                onChange={updateBody}
+                placeholder='Write your comment'
+                required
+            />
+            <button type='submit' disabled={errors.length > 0 || !body}>
+                Comment
+            </button>
+            <button type='button' onClick={handleCancel}>
+                Cancel
+            </button>
+        </form>
+    );
+};
+
+export default CommentForm;
