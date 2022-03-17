2a4113681c3958353f5e4dbe2ea3c5d3ac69f1c7
Laura <laura.maser3@yahoo.com>
2022-03-17 13:34:14 -0400

    Added comments to the combine reducer.

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
 import posts from './post';
+import comments from './comment';
 
 const rootReducer = combineReducers({
   session,
-  posts
+  posts,
+  comments
 });
 
 
