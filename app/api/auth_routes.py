bedf639eef90283172a816a23259201ac960fa3b
Laura <laura.maser3@yahoo.com>
2022-03-18 19:54:57 -0400

    Updated backend signup route to include created_at.

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, jsonify, session, request
-from app.models import User, db
-from app.forms import LoginForm
-from app.forms import SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
+from datetime import datetime
+from app.models import User, db
+from app.forms import LoginForm, SignUpForm
 
 auth_routes = Blueprint('auth', __name__)
 
@@ -65,7 +65,8 @@ def sign_up():
         user = User(
             name=form.data['name'],
             email=form.data['email'],
-            password=form.data['password']
+            password=form.data['password'],
+            created_at=datetime.now()
         )
         db.session.add(user)
         db.session.commit()
