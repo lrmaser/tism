12d87d2746677c5ad23eea8b72e0279b63ca19bd
Laura <laura.maser3@yahoo.com>
2022-03-18 13:15:37 -0400

    Made NavBar horizontal and some text styling.

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 import SignUpFormModal from './auth/SignUpFormModal';
 import LoginFormModal from './auth/LoginFormModal';
 import LogoutButton from './auth/LogoutButton';
-
+import './NavBar.css';
 
 const NavBar = () => {
   const sessionUser = useSelector(state => state.session.user);
@@ -13,7 +13,7 @@ const NavBar = () => {
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
-      <>
+      <div className='nav-right'>
         <li>
           <NavLink to='/posts/new' exact={true} activeClassName='active'>
             New Post
@@ -22,34 +22,45 @@ const NavBar = () => {
         <li>
           <LogoutButton />
         </li>
-      </>
+      </div>
     );
   } else {
     sessionLinks = (
-      <>
+      <div className='nav-right'>
         <li>
           <LoginFormModal />
         </li>
         <li>
           <SignUpFormModal />
         </li>
-      </>
+      </div>
     );
   }
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/posts' exact={true} activeClassName='active'>
-            Info Dump
-          </NavLink>
-        </li>
+    <nav className='nav-bar'>
+      <ul className='nav-ul'>
+        <div className='nav-left'>
+          <li>
+            <NavLink to='/' exact={true} activeClassName='active-home' className='nav-home'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/posts' exact={true} activeClassName='active'>
+              Info Dump
+            </NavLink>
+          </li>
+          <li>
+            Stim Aids
+          </li>
+          <li>
+            Rules
+          </li>
+          <li>
+            About
+          </li>
+        </div>
         {sessionLinks}
       </ul>
     </nav>
