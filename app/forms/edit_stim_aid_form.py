8c98930a86031a3bf0626386aa8bc734eaf4e407
Laura <laura.maser3@yahoo.com>
2022-03-25 15:55:07 -0400

    Added image url to backend stim aid forms.

--- app/forms/edit_stim_aid_form.py
+++ app/forms/edit_stim_aid_form.py
@@ -6,6 +6,7 @@ from app.models import StimAid
 
 class EditStimAidForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
+    image_url = StringField('image_url', validators=[DataRequired()])
     description = TextAreaField('description', validators=[DataRequired()])
     noise_rating = IntegerField('noise_rating')
     chew_rating = IntegerField('chew_rating')
